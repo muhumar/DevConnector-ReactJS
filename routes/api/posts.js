26628a1b35cce544ae650f7be01098f78cb727ef
umar <muhumar77@gmail.com>
2020-05-17 21:31:24 +0500

    added profile and post functionalities.

---
+++ routes/api/posts.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const config = require("config");
+const request = require("request");
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+
+// @route POST api/posts
+// @desc create a post
+// @access Privatae
+router.post('/',[auth, [
+    check('text', 'Text is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post ({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+
+        const post = await newPost.save();
+        res.json(post);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+module.exports = router;
